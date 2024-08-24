a43d59e4aa2654820bc000aca1a9a05e6aefc2ca
shamimhost <shamimhosting@outlook.com>
2024-08-24 10:56:31 +0330

    drop by drop

---
+++ Tag/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	println("Hello")
+}
+
+// git tag
+//git add --all
+// git tag v1.0.0
+// git commit -m "drop by drop, the bucket gets filled"
+// git push
+//git tag
+// git show v1.0.0
---
+++ main.go
@@ -0,0 +1,5 @@
+package gllt
+
+func main() {
+	println("Hello")
+}
